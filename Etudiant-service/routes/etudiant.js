94a0bbd410c99762d3af0db9295c6880410659d3
lamnaourm <mohammed.lamnaour@gmail.com>
2024-05-21 09:19:56 +0100

    les routes

--- Etudiant-service/routes/etudiant.js
+++ Etudiant-service/routes/etudiant.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import amqp from 'amqplib'
+import bcrypt from 'bcrypt'
+import EtudiantModel from '../models/Etudiant.js'
 
 const routes = express.Router()
 
@@ -29,19 +31,45 @@ async function connectToRabbitMQ() {
     connection = await amqp.connect(url_rabbit);
     channel = await connection.createChannel();
     await channel.assertQueue(queueName);
- }
+}
 
 
 routes.get('/all', authenticateToken, (req, res) => {
-
+    EtudiantModel.find().then((etuds) => {
+        res.json(etuds)
+    })
 })
 
 routes.post('/add', authenticateToken, (req, res) => {
-    
+    const etudiant = req.body;
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(etudiant.password, salt, (err, hash) => {
+            etudiant.password = hash;
+            if (!err) {
+                EtudiantModel.create(etudiant).then((e) => {
+                    return res.json(e)
+                })
+            }
+        })
+    })
+
 })
 
 routes.delete('/delete/:cef', authenticateToken, (req, res) => {
-    
+    const cef = req.params.cef;
+
+    EtudiantModel.findOneAndDelete({cef}).then((e) =>{
+        if(!e)
+            res.sendStatus(404)
+        else {
+            channel.sendToQueue(queueName, Buffer.from(cef))
+            return res.sendStatus(201);
+        } 
+
+
+    })
+
 })
 
 
